289a0c763f25d67106268b17539279d7a5c5b2f7
WellingtonJr <wferreira.bm13@gmail.com>
2023-03-25 16:11:44 -0300

    Configurando o Resource Server com a nova stack do Spring Security

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+## IMPL DA API COURSE
+- Criar projeto Spring pelo initialzr, importando spring web, jpa, lombok e postgres
---
+++ src/main/java/com/ead/course/security/ResourceServerConfig.java
@@ -0,0 +1,23 @@
+package com.ead.course.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer().opaqueToken();
+    }
+
+}
